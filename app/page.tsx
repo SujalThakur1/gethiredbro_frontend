c6ec83c5b4b8445834b969c534480e78ced5dfbf
khushxxl <khushaal.choithramani@gmail.com>
2026-01-12 19:51:47 +0000

    initializing nextjs

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+    <div className="flex items-center justify-center min-h-screen">
+      <h1 className="text-3xl font-bold text-center">
+        Get Hired Bro
+      </h1>
+    </div>
+    </>
+  );
+}
